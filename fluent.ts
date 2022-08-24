4fdb17cc7999e40297666e9809b89e7e95a13f12
Alexander Bukhalo <alex@hypeer.company>
2022-08-24 19:25:36 +0300

    i18n support

---
+++ fluent.ts
@@ -0,0 +1,26 @@
+import { Fluent, useFluent } from "./deps.ts";
+// import { BotContext } from "./context.ts";
+
+const fluent = new Fluent();
+
+export const fluentMiddleware = useFluent({
+  fluent,
+  // localeNegotiator: (ctx: BotContext) => {
+  //   if (!ctx.session.locale) {
+  //     return ctx.session.locale = ctx.from?.language_code || "en";
+  //   }
+  //
+  //   return ctx.session.locale;
+  // },
+});
+
+export const setup = async () => {
+  await fluent.addTranslation({
+    locales: "en",
+    filePath: [`./i18n/translation.en.ftl`],
+  });
+  await fluent.addTranslation({
+    locales: "ru",
+    filePath: [`./i18n/translation.ru.ftl`],
+  });
+};
